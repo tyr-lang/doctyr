d67efe75fccac5d5ffdb552daf9daa61decef9bc
Dr. Timm Felden <timm.felden@felden.com>
2024-09-14 20:53:39 +0200

    add basic tags support

--- src/doctyr/IndexWriter.java
+++ src/doctyr/IndexWriter.java
@@ -7,23 +7,41 @@ import tyr.tooling.sdk.ast.OGFile;
 
 public class IndexWriter {
 
-   public IndexWriter(OGFile sg) {
-      try (var out = new FileWriter("mkdocs.yml")) {
-         out.write("site_name: \"" + String.join(".", sg.Packages.get().getName()) + "\"\n");
-         out.write("docs_dir: \"docs\"\n");
-         out.write("use_directory_urls: false\n");
-         out.write("theme:\n" + "  name: mkdocs\n" + "  color_mode: dark\n");
+	public IndexWriter(OGFile sg) {
+		// touch tags to ensure it can be generated (whatever went wrong in python in
+		// that regard)
+		try (var out = new FileWriter("docs/tags.md")) {
+			// empty file
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-         out.write("nav:\n");
-         out.write("  - Home: index.md\n");
+		// create the configuration holding the actual index amongst others
+		try (var out = new FileWriter("mkdocs.yml")) {
+			out.write("site_name: \"" + String.join(".", sg.Packages.get().getName()) + "\"\n");
+			out.write("""
+docs_dir: \"docs\"
+plugins:
+  - tags:
+      tags_file: tags.md
+  - offline
+  - privacy
+theme:
+  name: material
+""");
 
-         out.write("  - types:\n");
-         for (var t : sg.TypeDefs)
-            out.write("    - \"" + TypeWriter.name(t) + "\": \"" + TypeWriter.docFileRelativeName(t) + "\"\n");
+			out.write("nav:\n");
+			out.write("  - Home: index.md\n");
 
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+			out.write("  - types:\n");
+			for (var t : sg.TypeDefs)
+				out.write("    - \"" + TypeWriter.name(t) + "\": \"" + TypeWriter.docFileRelativeName(t) + "\"\n");
+
+			out.write("  - Tags: tags.md\n");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/doctyr/TypeWriter.java
+++ src/doctyr/TypeWriter.java
@@ -5,37 +5,53 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import tyr.tooling.sdk.ast.OGFile;
+import tyr.tooling.sdk.ast.PublicVisible;
 import tyr.tooling.sdk.ast.TypeDef;
 
 public class TypeWriter {
 
-   public static final File base = new File("docs", "types");
+	public static final File base = new File("docs", "types");
 
-   public TypeWriter(OGFile sg) {
-      for (var t : sg.TypeDefs)
-         write(t);
-   }
+	public TypeWriter(OGFile sg) {
+		for (var t : sg.TypeDefs)
+			write(t);
+	}
 
-   private void write(TypeDef t) {
-      var f = docFile(t);
-      f.getParentFile().mkdirs();
-      try (var out = new FileWriter(f)) {
-         out.write("#description of type " + name(t) + "\n more text");
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+	private void write(TypeDef t) {
+		var f = docFile(t);
+		f.getParentFile().mkdirs();
+		try (var out = new FileWriter(f)) {
+			writeTags(t, out);
 
-   public static File docFile(TypeDef t) {
-      return new File(base, t.ID() + ".md");
-   }
+			out.write("#description of type " + name(t) + "\n more text");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-   public static String docFileRelativeName(TypeDef t) {
-      return docFile(t).toString().substring(5);
-   }
+	private void writeTags(TypeDef t, FileWriter out) throws IOException {
+		out.write("---\n");
+		out.write("tags:\n");
 
-   public static String name(TypeDef t) {
-      return t.getName();
-   }
+		if (t instanceof TypeDef)
+			out.write("  - type\n");
+
+		if (t.getVisibility() instanceof PublicVisible)
+			out.write("  - public\n");
+
+		out.write("---\n");
+	}
+
+	public static File docFile(TypeDef t) {
+		return new File(base, t.ID() + ".md");
+	}
+
+	public static String docFileRelativeName(TypeDef t) {
+		return docFile(t).toString().substring(5);
+	}
+
+	public static String name(TypeDef t) {
+		return t.getName();
+	}
 
 }
