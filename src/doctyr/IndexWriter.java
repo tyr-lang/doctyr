09a0b2c447d33d4cf996717738b5d69038c921c2
Dr. Timm Felden <timm.felden@felden.com>
2024-09-26 20:46:59 +0200

    start customizing colors to match branding

--- src/doctyr/IndexWriter.java
+++ src/doctyr/IndexWriter.java
@@ -1,5 +1,6 @@
 package doctyr;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,6 +18,33 @@ public class IndexWriter {
 			// empty file
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		
+		// color scheme
+		{
+			var f = new File("docs/stylesheets/extra.css");
+			f.getParentFile().mkdirs();
+			try (var out = new FileWriter(f)) {
+				out.write("""
+[data-md-color-scheme="tyr"] {
+  --md-primary-fg-color:        #092147;
+  --md-primary-fg-color--light: #1a488e;
+  --md-primary-fg-color--dark:  #00193e;
+}
+[data-md-color-scheme="tyr-dark"] {
+  --md-primary-fg-color:        #092147;
+  --md-primary-fg-color--light: #1a488e;
+  --md-primary-fg-color--dark:  #00193e;
+
+  --md-accent-fg-color:                rgba(0, 0, 1, 1);
+  --md-accent-fg-color--transparent:   rgba(0, 0, 1, 0.1);
+  --md-accent-bg-color:                rgba(0, 0, 0, 1);
+  --md-accent-bg-color--light:         rgba(0, 0, 0, 0.7);
+}
+""");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		// copy logo
@@ -37,22 +65,20 @@ plugins:
   - offline
   - privacy
   - search
+extra_css:
+  - stylesheets/extra.css
 theme:
   name: material
   logo: logo.png
   favicon: logo.png
   palette:
     # Palette toggle for dark mode
-    - scheme: slate
-      primary: amber
-      accent: yellow
+    - scheme: tyr-dark
       toggle:
         icon: material/brightness-4
         name: Switch to light mode
     # Palette toggle for light mode
-    - scheme: default
-      primary: blue
-      accent: light blue
+    - scheme: tyr
       toggle:
         icon: material/brightness-7 
         name: Switch to dark mode
