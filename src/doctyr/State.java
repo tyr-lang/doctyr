b5d0d43c1be32044caecd75ad2603683a55fc6bd
Timm Felden <timm.felden@felden.com>
2025-11-30 17:19:09 +0100

    fix doc generation failing if out dir does not exist

--- src/doctyr/State.java
+++ src/doctyr/State.java
@@ -13,35 +13,36 @@ import tyr.tooling.sdk.ast.OGFile;
  */
 public class State implements Closeable {
 
-	private final OGFile sg;
-	private final File tmp;
-	private final File out;
+   private final OGFile sg;
+   private final File tmp;
+   private final File out;
 
-	public State(String inPath, File tmp, File out) throws OGSSException, IOException {
-		sg = OGFile.open(inPath, Mode.Read, Mode.ReadOnly);
-		this.tmp = tmp;
-		this.out = out;
-	}
+   public State(String inPath, File tmp, File out) throws OGSSException, IOException {
+      out.mkdirs();
+      sg = OGFile.open(inPath, Mode.Read, Mode.ReadOnly);
+      this.tmp = tmp;
+      this.out = out;
+   }
 
-	@Override
-	public void close() throws IOException {
-		sg.close();
-	}
+   @Override
+   public void close() throws IOException {
+      sg.close();
+   }
 
-	public void prepare() {
-		new PackageWriter(sg);
-		new TypeWriter(sg);
-		new IndexWriter(sg);
-	}
+   public void prepare() {
+      new PackageWriter(sg);
+      new TypeWriter(sg);
+      new IndexWriter(sg);
+   }
 
-	public void generate() throws IOException {
-		var pb = new ProcessBuilder("mkdocs", "build").inheritIO();
-		var p = pb.start();
-		try {
-			p.waitFor();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+   public void generate() throws IOException {
+      var pb = new ProcessBuilder("mkdocs", "build").inheritIO();
+      var p = pb.start();
+      try {
+         p.waitFor();
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+   }
 
 }
